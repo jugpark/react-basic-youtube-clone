06636f020c0c2424a0a268bbc99e316d82e9d6b2
박주광 <parkjugwang@bagjugwangs-MacBook-Pro.local>
2022-01-22 21:24:51 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,27 @@
 import './app.css';
-import React from 'react';
-import Habit from './components/habit';
+import React, { Fragment, useState, useEffect } from 'react';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return 
-    <Habit />
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDMdHIAD23dal7E5UGBGPXkPLRHtqfT5pA", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return (
+    <Fragment>
+      <VideoList videos={videos} />
+    </Fragment>
+  )
 }
 
 export default App;
