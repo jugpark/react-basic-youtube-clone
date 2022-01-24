427a5940eb9955137281650d584c94c1f09e37f3
박주광 <parkjugwang@bagjugwangs-MacBook-Pro.local>
2022-01-24 23:09:23 +0900

    youtube clone final

--- src/app.jsx
+++ src/app.jsx
@@ -2,26 +2,44 @@ import React, { Fragment, useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 import styles from "./app.module.css";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
   const search = (query) => {
     youtube
       .search(query) //
-      .then(videos => setVideos(videos));
+      .then(videos => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
     youtube
       .mostPopular() //
-      .then(videos => setVideos(videos));
+      .then((videos) => setVideos(videos));
   }, [youtube]);
 
   return (
     <Fragment>
       <div className={styles.app}>
         <SearchHeader onSearch={search} />
-        <VideoList videos={videos} />
+        <section className={styles.content}>
+          {selectedVideo && (
+            <div className={styles.detail}>
+              <VideoDetail video={selectedVideo} />
+            </div>
+          )}
+          <div className={styles.list}>
+            <VideoList videos={videos} onVideoClick={selectVideo} display={ selectedVideo? 'list' : 'grid'}/>
+          </div>
+        </section>
       </div>
     </Fragment>
   );
